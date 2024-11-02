ac18832fc61a0628a9ce65692802eaef995b5a72
Shahbaaz Buksh <shahbaazbuksh@yahoo.com>
2024-11-02 21:19:05 +0530

    Commit 434

--- routes/blog.js
+++ routes/blog.js
@@ -8,8 +8,13 @@ router.get("/", function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/posts", function (req, res) {
-  res.render("posts-list");
+router.get("/posts", async function (req, res) {
+  const query = `
+        SELECT posts.*, authors.name AS author_name FROM posts 
+        INNER JOIN authors ON posts.author_id = authors.id
+    `;
+  const [posts] = await db.query(query);
+  res.render("posts-list", { posts: posts });
 });
 
 router.get("/new-post", async function (req, res) {
@@ -29,6 +34,21 @@ router.post("/posts", async function (req, res) {
     [data]
   );
   res.redirect("/posts");
+});
+
+router.get("/posts/:id", async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+  `;
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render("404");
+  }
+
+  res.render("post-detail", { post: posts[0] });
 });
 
 module.exports = router;
