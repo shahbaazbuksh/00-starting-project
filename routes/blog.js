6d5aa82da93125e2712411e4d0768a801fe1d5ee
Shahbaaz Buksh <shahbaazbuksh@yahoo.com>
2024-11-02 21:28:31 +0530

    434

--- routes/blog.js
+++ routes/blog.js
@@ -48,7 +48,18 @@ router.get("/posts/:id", async function (req, res) {
     return res.status(404).render("404");
   }
 
-  res.render("post-detail", { post: posts[0] });
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    humanReadableDate: posts[0].date.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  };
+
+  res.render("post-detail", { post: postData });
 });
 
 module.exports = router;
